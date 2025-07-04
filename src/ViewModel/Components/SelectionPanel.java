67e8d1a392cc695f863c89dc1bf7b7de519514b2
Adam Long <adam.jacob.long@gmail.com>
2025-07-03 23:10:13 -0500

    Implement template code

--- src/ViewModel/Components/SelectionPanel.java
+++ src/ViewModel/Components/SelectionPanel.java
@@ -1,8 +1,17 @@
 package ViewModel.Components;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class SelectionPanel extends JPanel
 {
+   public final JButton renameBtn = new JButton("Rename");
+   public final JButton deleteBtn = new JButton("Delete");
 
+   public SelectionPanel(ActionListener renameAction, ActionListener deleteAction) {
+      add(renameBtn);
+      add(deleteBtn);
+      renameBtn.addActionListener(renameAction);
+      deleteBtn.addActionListener(deleteAction);
+   }
 }
