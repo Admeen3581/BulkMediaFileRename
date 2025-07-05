2efe4b2b8e2acd9fb95a852f41b6a17bcf0220db
Adam Long <adam.jacob.long@gmail.com>
2025-07-05 11:45:38 -0500

    Add controller and actions for MasterFrame buttons

---
+++ src/ViewModel/Controllers/MasterFrameController.java
@@ -0,0 +1,16 @@
+package ViewModel.Controllers;
+
+import javafx.event.ActionEvent;
+
+public class MasterFrameController
+{
+   public void rename(ActionEvent event)
+   {
+      System.out.println( "Hello World!");
+   }
+
+   public void delete(ActionEvent event)
+   {
+
+   }
+}
