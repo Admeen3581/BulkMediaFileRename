e2df3a4e035d4e0204fdecfb52e61a73a8a16b6c
Adam Long <adam.jacob.long@gmail.com>
2025-07-05 12:18:49 -0500

    Add MediaView support and refactor MasterFrame buttons and layout

--- src/ViewModel/Controllers/MasterFrameController.java
+++ src/ViewModel/Controllers/MasterFrameController.java
@@ -1,16 +1,44 @@
 package ViewModel.Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 
-public class MasterFrameController
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MasterFrameController implements Initializable
 {
-   public void rename(ActionEvent event)
+   @FXML
+   private MediaView mediaViewer;
+   private Button renameButton, deleteButton;
+
+   private File file;
+   private Media media;
+   private MediaPlayer mediaPlayer;
+
+   @Override
+   public void initialize(URL url, ResourceBundle resourceBundle)
    {
-      System.out.println( "Hello World!");
+      this.file = new File("assets/AUPJ8534.mp4");
+      this.media = new Media(file.toURI().toString());
+      this.mediaPlayer = new MediaPlayer(media);
+      this.mediaPlayer.setAutoPlay(true);
+      this.mediaViewer.setMediaPlayer(mediaPlayer);
    }
 
-   public void delete(ActionEvent event)
+   public void renameButtonTrigger(ActionEvent event)
    {
+      System.out.println( "Hello World! Rename");
+   }
 
+   public void deleteButtonTrigger(ActionEvent event)
+   {
+      System.out.println( "Hello World! Delete");
    }
 }
