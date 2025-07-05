4f55fbb411e70d8d9c9411c05b1041b52a27f571
Adam Long <adam.jacob.long@gmail.com>
2025-07-05 12:59:18 -0500

    Add image and video display support to MasterFrame with dynamic file type handling

--- src/ViewModel/Controllers/MasterFrameController.java
+++ src/ViewModel/Controllers/MasterFrameController.java
@@ -3,7 +3,8 @@ package ViewModel.Controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -15,30 +16,142 @@ import java.util.ResourceBundle;
 public class MasterFrameController implements Initializable
 {
    @FXML
-   private MediaView mediaViewer;
-   private Button renameButton, deleteButton;
+   private MediaView videoViewer;
+   private ImageView imageViewer;
 
-   private File file;
-   private Media media;
-   private MediaPlayer mediaPlayer;
+   private File file;//doesn't support .MOV
 
+   /**
+    * Initializes the controller and sets up media display functionality.
+    *
+    * This method is called after the FXML loader has fully initialized the scene. It attempts
+    * to load a media file and determine its type based on the file extension. Supported file
+    * types include videos (e.g., ".mp4") and images (e.g., ".gif", ".jpg", ".png", ".jpeg").
+    * It then displays the appropriate media type in the respective viewer component. If the
+    * file type is unsupported, no media is displayed, and an error message is logged.
+    *
+    * @param url the location used to resolve relative paths for the root object
+    * @param resourceBundle the resource bundle to localize the root object
+    */
    @Override
    public void initialize(URL url, ResourceBundle resourceBundle)
    {
-      this.file = new File("assets/AUPJ8534.mp4");
-      this.media = new Media(file.toURI().toString());
-      this.mediaPlayer = new MediaPlayer(media);
-      this.mediaPlayer.setAutoPlay(true);
-      this.mediaViewer.setMediaPlayer(mediaPlayer);
+      try
+      {
+         this.file = new File("assets/AUPJ8534.mp4");//hardcoded for testing
+         if (!file.exists())
+         {
+            System.err.println("Media file not found: " + file.getAbsolutePath());
+            return;
+         }
+         String fileName = this.file.getName();
+
+         if (isVideo(fileName))
+         {
+            displayVideo();
+         }
+
+         else if(isImage(fileName))
+         {
+            displayImage();
+         }
+         else
+         {
+            System.err.println("Unsupported file type: " + fileName);
+            videoViewer.setVisible(false);
+            imageViewer.setVisible(false);
+         }
+      }
+      catch (Exception e)
+      {
+         System.err.println("Error initializing media player: " + e.getMessage());
+         e.printStackTrace();
+      }
    }
 
+   /**
+    * Handles the action event triggered by pressing the rename button.
+    *
+    * @param event the ActionEvent object that represents the user interaction with the rename button
+    */
+   @FXML
    public void renameButtonTrigger(ActionEvent event)
    {
       System.out.println( "Hello World! Rename");
    }
 
+   /**
+    * Handles the action event triggered by pressing the delete button.
+    *
+    * @param event the ActionEvent object that represents the user interaction with the delete button
+    */
+   @FXML
    public void deleteButtonTrigger(ActionEvent event)
    {
       System.out.println( "Hello World! Delete");
+   }
+
+   /**
+    * Displays a video in the assigned MediaView component.
+    *
+    * This method initializes a Media object with the file URI, creates a MediaPlayer to play the video,
+    * and associates the MediaPlayer with the MediaView component (`videoViewer`). The video is
+    * configured to autoplay and loop indefinitely. If an image is currently being displayed in the
+    * ImageView component (`imageViewer`), it is hidden to ensure only the video is visible.
+    */
+   private void displayVideo()
+   {
+      Media media = new Media(file.toURI().toString());
+      MediaPlayer player = new MediaPlayer(media);
+      player.setAutoPlay(true);
+      player.setCycleCount(MediaPlayer.INDEFINITE);
+      this.videoViewer.setMediaPlayer(player);
+
+      this.videoViewer.setVisible(true);
+      if(imageViewer != null)
+         this.imageViewer.setVisible(false);
+   }
+
+   /**
+    * Displays an image in the assigned ImageView component.
+    *
+    * This method initializes an Image object with the URI of the file, sets it
+    * to the ImageView component (`imageViewer`), and ensures the component is
+    * visible. If a video is currently being displayed in the MediaView component
+    * (`videoViewer`), it is hidden to ensure only the image is visible.
+    */
+   private void displayImage()
+   {
+      Image image = new Image(file.toURI().toString());
+      this.imageViewer.setImage(image);
+
+      this.imageViewer.setVisible(true);
+      if(videoViewer != null)
+         this.videoViewer.setVisible(false);
+   }
+
+   /**
+    * Determines whether the given file name corresponds to a video file.
+    *
+    * @param fileName the name of the file to be checked
+    * @return true if the file name ends with ".mp4", false otherwise
+    */
+   private boolean isVideo(String fileName)
+   {
+      return fileName.endsWith(".mp4");
+   }
+
+   /**
+    * Determines whether the given file name corresponds to an image file.
+    *
+    * @param fileName the name of the file to be checked
+    * @return true if the file name ends with ".gif", ".jpg", ".png", or ".jpeg", false otherwise
+    */
+   private boolean isImage(String fileName)
+   {
+      return fileName.endsWith(".gif") ||
+             fileName.endsWith(".jpg") ||
+             fileName.endsWith(".png") ||
+             fileName.endsWith(".jpeg");
    }
 }
