d28f212b1ce0f927493cd98ab9e30f0553af35a3
Adam Long <adam.jacob.long@gmail.com>
2025-07-05 20:24:37 -0500

    Remove `NameProviders` package and all related classes/interfaces.

--- src/NameProviders/NameProvider.java
+++
@@ -1,9 +0,0 @@
-package NameProviders;
-
-import java.io.File;
-
-public interface NameProvider {
-    public void resetState ();
-    public void inferOldState ();
-    public String getName (File file);
-}
